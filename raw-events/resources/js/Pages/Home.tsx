df885112bd931c3ee0df1af03b1987cb5e4b721c
trusted2care <phil@trusted2care.com>
2025-01-16 07:52:29 +0000

    Changed web.php to change route to new Home page. Made an alias '@' for "resources/js"

---
+++ raw-events/resources/js/Pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../css/app.css';
+
+export default function Home(){
+    return (
+        <div>
+            <h1>Hello User</h1>
+        </div>
+    );
+}
--- raw-events/tailwind.config.js
+++ raw-events/tailwind.config.js
@@ -3,10 +3,10 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',,
         './resources/**/*.blade.php',
         './resources/**/*.js',
+        './resources/**/*.tsx',
         './resources/**/*.vue',
     ],
     theme: {
